a1fc14374fe02981875f80ac2f6523d4462172a6
hlotof <hlotof@gmail.com>
2018-06-12 03:12:56 +0300

    Create web__arab48_com.py
    Arab news website

---
+++ services/web__arab48_com.py
@@ -0,0 +1,9 @@
+refresh = 1800
+version = 20180612.01
+
+urls = ['https://www.arab48.com',
+  'https://www.arab48.com/%D9%81%D8%B3%D8%AD%D8%A9',
+	'https://www.arab48.com/rss']
+regex = [r'^https?:\/\/[^\/]*arab48\.com\/']	
+videoregex = []
+liveregex = []
